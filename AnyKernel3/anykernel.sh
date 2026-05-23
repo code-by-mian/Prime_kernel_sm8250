59d35dab08a020c4f6bc09a7840c6f833b0eb0f3
code-by-mian <codedbymian@gmail.com>
2026-05-23 15:22:26 +0500

    Update anykernel.sh

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Kernel for S20 FE (Snapdragon) by pascua28 @ xda-developers
+kernel.string=Kernel for S20 N20 Series (Snapdragon) by pascua28 Mian Hamza @ xda-developers 
 do.devicecheck=1
 do.modules=0
 do.systemless=1
