710413769f466e2ba0193f7eac427babf28cecf7
code-by-mian <codedbymian@gmail.com>
2026-05-24 12:48:24 +0500

    Update build_boot.sh

--- build_boot.sh
+++ build_boot.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-export DEVICE=$1
+KERNEL_DIR=$(pwd)
+DEVICE="$1"
 
 build_kernel() {
     echo "-----------------------------------------------"
-    echo "Beginning kernel compilation..."
+    echo "Beginning kernel compilation for $DEVICE..."
     echo "-----------------------------------------------"
 
     export ARCH=arm64
@@ -14,18 +15,18 @@ build_kernel() {
 
     BUILD_VAR="-j$(nproc) -C $(pwd) O=$(pwd)/out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1"
 
-    # NO_QCACLD, CNSS_WORKAROUND, SEC_PCIE*: see vendor/samsung/${DEVICE}.config
-    cat arch/arm64/configs/vendor/kona-sec-perf_defconfig arch/arm64/configs/vendor/samsung/${DEVICE}.config \
-        arch/arm64/configs/ksu.config arch/arm64/configs/vendor/not/no_werror.config \
-        arch/arm64/configs/vendor/debugfs.config > arch/arm64/configs/temp_defconfig
+    cat arch/arm64/configs/vendor/kona-sec-perf_defconfig arch/arm64/configs/vendor/samsung/$DEVICE.config \
+        arch/arm64/configs/ksu.config > arch/arm64/configs/temp_defconfig
 
     echo "
-    CONFIG_THINLTO=y
-    # CONFIG_LTO_NONE is not set
-    CONFIG_LTO_CLANG=y
-    " >> arch/arm64/configs/temp_defconfig
+CONFIG_THINLTO=y
+# CONFIG_LTO_NONE is not set
+CONFIG_LTO_CLANG=y
 
-    make $BUILD_VAR temp_defconfig || exit 1
+CONFIG_LOCALVERSION=\"-AstroKernel\"
+" >> arch/arm64/configs/temp_defconfig
+
+    make $BUILD_VAR temp_defconfig
     rm arch/arm64/configs/temp_defconfig
 }
 
