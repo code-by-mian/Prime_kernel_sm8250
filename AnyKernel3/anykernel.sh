d6b1b0fa1c1a7d70db28efe5d5eae66c8a6c2b7c
codebymian <codebymian@gmail.com>
2026-05-24 09:52:23 +0500

    fixed -werror + build

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -5,16 +5,17 @@
 # begin properties
 properties() { '
 kernel.string=Kernel for S20 FE (Snapdragon) by pascua28 @ xda-developers
-do.devicecheck=1
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=r8q
-device.name2=r8qxx
-device.name3=r8qxxx
-device.name4=
-device.name5=
+device.name2=y2q
+device.name3=x1q
+device.name4=z3q
+device.name5=c2q
+device.name6=c1q
 supported.versions=
 supported.patchlevels=
 '; } # end properties
