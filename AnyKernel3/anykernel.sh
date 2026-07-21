e5311b1da1746d972bcaeea63fc02ee7289526f0
AstroByteX-Code <codebymianhamza@gmail.com.com>
2026-07-21 10:48:51 +0500

    added all devices support

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,17 +4,17 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Kernel for S20 plus (Snapdragon) by pascua28 mianhamza @ xda-developers
-do.devicecheck=1
+kernel.string=Kernel for S20 Series (Snapdragon) by pascua28 mianhamza @ xda-developers
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=y2q
-device.name2=y2qxx
-device.name3=y2qxxx
-device.name4=
-device.name5=
+device.name1=x1q
+device.name2=y2q
+device.name3=z3q
+device.name4=c1q
+device.name5=c2q
 supported.versions=
 supported.patchlevels=
 '; } # end properties
